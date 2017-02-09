8502fa445c8a8c82df189f9ac55ff12bd3e23f86
setsumi <kreliahn@gmail.com>
2017-02-09 11:12:18 +0900

    fix mecab

--- server.py
+++ server.py
@@ -179,7 +179,7 @@ class Mecab(object):
 
     def __init__(self):
         self.dictionary_format = Mecab.UNIDIC
-        self.args = ['mecab', '-d', 'data/unidic-mecab-translate']
+        self.args = ['bin/mecab.exe', '-d', 'data/unidic-mecab-translate']
         self.process = Popen(self.args, stdout=PIPE, stdin=PIPE, bufsize=1)
         self.output = Queue()
         self.t = Thread(target=self._handle_stdout)
